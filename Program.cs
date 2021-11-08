ab97188d136e7fe9307ffa5e8558e9e3e1652e7b
Amanda Shelton <ashelton@reynolds.edu>
2021-11-08 08:04:49 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DebugQuiz5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayWebAddress;
+            Writeline("Shop at Shopper's World");
+            DisplayWebAddress;
+            WriteLine("The best bargains from around the world");
+            DisplayWebAddres;
+        }
+        public void DisplayWebAddress()
+        {
+            WriteLine("------------------------------");
+            WriteLine("Visit us on the web at:");
+            WriteLine("www.shoppersworldbargains.com");
+            WriteLine("******************************");
+        }
+    }
